14884ce8bc4923106c8bb838810362a848ab6160
arifnayan2861 <am.nayan2861@gmail.com>
2023-11-20 10:40:16 +0600

    update-2

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const {
   errorHandler,
 } = require("./middlewares/common/errorHandler");
 const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
 
 const app = express();
 dotenv.config();
@@ -36,7 +38,8 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //Routing setup
 app.use("/", loginRouter);
-//
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
 //404 not found handler
 app.use(notFoundHandler);
